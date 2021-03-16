912b7f0dc4c69fca6fa4e06536c7c63f6fdc5d6d
josepereza <josepereza66@gmail.com>
2021-03-16 13:03:12 +0100

    first2 commit

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict'
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require("path");
+var port = process.env.PORT || 3000;
+var counter=0;
+//var counter=global.counter;
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", function(req,res,next) {
+	res.redirect("/");
+})
+
+ 
+io.on('connection', function(socket){
+	const user = socket.handshake.query.name;
+	const from = socket.id;
+	counter++;
+    
+	socket.on('chat_message_sent', function(msg){
+		io.emit('chat_message_received', { ...msg, user, from});
+	});
+
+	socket.on('confetti_thrown', function(msg){
+		io.emit('confetti_received',{ from, user});
+	});
+
+	socket.on('disconnect', function(msg){
+		counter--;
+		io.emit('member_exit', { from, user,counter });
+	});
+
+	io.emit('new_member', { from, user, counter});
+});
+
+http.listen(port, function(){
+  console.log('Open your browser on http://localhost:' + port);
+});
+
+
+
